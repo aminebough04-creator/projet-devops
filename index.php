52a157794dc6c16231d485684345fc18e247be9c
Amine <amine@gmail.com>
2026-05-20 20:59:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -162,7 +162,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <div class="login-logo">
         <i class="bi bi-shield-lock-fill"></i>
       </div>
-      <h2>Gestion des Besoins 5</h2>
+      <h2>Gestion des Besoins 6</h2>
       <p>Connectez-vous à votre compte</p>
     </div>
     <div class="login-body">
